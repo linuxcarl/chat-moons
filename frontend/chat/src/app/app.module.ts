65f4c459089127c001549f475a06edfaa9cc501a
Carlos Ramirez <carlos@extrasistemas.com>
2020-08-01 14:07:01 -0500

    added socketio and chatServices

--- frontend/chat/src/app/app.module.ts
+++ frontend/chat/src/app/app.module.ts
@@ -3,14 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { environment } from 'src/environments/environment';
+import { SocketIoModule, SocketIoConfig } from 'ngx-socket-io';
+const config: SocketIoConfig = { url: environment.apiUrl, options: {} };
+ 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SocketIoModule.forRoot(config)
   ],
   providers: [],
   bootstrap: [AppComponent]

9484b990a3f4fcfc919e74b6bb0c97d192e01e40
Carlos Ramirez <carlos@extrasistemas.com>
2020-08-01 14:21:49 -0500

    added interface message
    added methods in chat service by get and save messages

--- frontend/chat/src/app/services/chat.service.ts
+++ frontend/chat/src/app/services/chat.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
-
+import urljoin from 'url-join';
+import { environment } from 'src/environments/environment';
+import { HttpHeaders, HttpClient } from '@angular/common/http';
+import { Message } from '../interface/message';
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ChatService {
-
-  constructor() { }
+  url = urljoin(environment.apiUrl, 'messages');
+  headers = new HttpHeaders({ 'Content-Type': 'application/json' });
+  constructor(private http: HttpClient) {}
+  getMessage(){
+    return this.http.get(this.url);
+  }
+  saveMessage($data: Message) {
+    return this.http.post(this.url, $data, { headers: this.headers });
+  }
 }

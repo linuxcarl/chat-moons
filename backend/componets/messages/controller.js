14646e056154afddde8dfc09e35175a5fb7d1e2d
Carlos Ramirez <carlos@extrasistemas.com>
2020-08-01 00:53:36 -0500

    added socketIO in the controller

--- backend/componets/messages/controller.js
+++ backend/componets/messages/controller.js
@@ -1,14 +1,17 @@
 const store = require('./store');
+const { socket } = require('../../socket');
+
 async function addMessage(FormData = Object()) {
-    FormData.date = new Date();
-    return await store.create(FormData);
+  FormData.date = new Date();
+  const message = await store.create(FormData);
+  socket.io.emit('message', message);
 }
 
 async function getMessages() {
-    return await store.getAll();
+  return await store.getAll();
 }
 
 module.exports = {
-    addMessage,
-    getMessages
-}
+  addMessage,
+  getMessages,
+};
